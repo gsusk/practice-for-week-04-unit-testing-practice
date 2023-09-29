d6b544d86b0c490cfcb297e60bda7e01308b14a3
gsusk <satehortuamercado@gmail.com>
2023-09-28 19:33:45 -0500

    add class specs

--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,60 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let word;
+
+  before(() => {
+    word = new Word("Mario")
+  });
+
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.exist;
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.equal("Mario");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      const newWord = word.removeVowels();
+      expect(newWord).to.deep.equal('Mr');
+
+      const noVowel = new Word('try');
+      const noVowels = noVowel.removeVowels();
+      expect(noVowels).to.deep.equal('try');
+
+      const onlyVowel = new Word('aeiou');
+      const onlyVowels = onlyVowel.removeVowels();
+      expect(onlyVowels).to.deep.equal('');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      const newWord = word.removeConsonants();
+      expect(newWord).to.deep.equal('aio');
+
+      const noConsonants = new Word('eaui');
+      const noConsWord = noConsonants.removeConsonants();
+      expect(noConsWord).to.deep.equal('eaui');
+
+      const fullConsonants = new Word('xrlrtx');
+      const fullConsWord = fullConsonants.removeConsonants();
+      expect(fullConsWord).to.deep.equal('')
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      const newWord = word.pigLatin();
+      expect(newWord).to.be.deep.equal('arioMay');
+
+      const vowelAtStart = new Word('ant');
+      const vowelsAtStart = vowelAtStart.pigLatin();
+      expect(vowelsAtStart).to.be.deep.equal('antyay')
     });
   });
 });
